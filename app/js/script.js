a4645e2e17f8d61c6e3b2a3d8743d1a19a1b14aa
Dawid Osiński <dawidosinski59@gmail.com>
2023-10-03 21:25:32 +0200

    mobile menu funcionality added

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,11 @@
-console.log('HELLO');
+const buttonOpen = document.querySelector(".header__nav--menu");
+const mobileMenu = document.querySelector(".mobileMenu");
+const buttonClose = document.querySelector(".mobileMenu__nav--menu");
 
-const test = () => {
-	console.log('this is a test');
-};
+buttonOpen.addEventListener("click", () => {
+	mobileMenu.classList.add("mobileMenu__open");
+});
+
+buttonClose.addEventListener("click", () => {
+	mobileMenu.classList.remove("mobileMenu__open");
+});
