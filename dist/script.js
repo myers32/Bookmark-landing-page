de7b6e9321901d742d66f66dac784433204cc616
Dawid Osiński <dawidosinski59@gmail.com>
2023-10-04 19:45:57 +0200

    test commit

---
+++ dist/script.js
@@ -0,0 +1,2 @@
+"use strict";var buttonOpen=document.querySelector(".header__nav--menu"),mobileMenu=document.querySelector(".mobileMenu"),buttonClose=document.querySelector(".mobileMenu__nav--menu");buttonOpen.addEventListener("click",(function(){mobileMenu.classList.add("mobileMenu__open")})),buttonClose.addEventListener("click",(function(){mobileMenu.classList.remove("mobileMenu__open")}));
+//# sourceMappingURL=script.js.map
