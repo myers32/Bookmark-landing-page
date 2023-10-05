ffc176bb61e9da7c47211a64b32dc09c29adaad2
Dawid Osiński <dawidosinski59@gmail.com>
2023-10-05 16:18:07 +0200

    bookmarks function added

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,57 @@
-const buttonOpen = document.querySelector(".header__nav--menu");
-const mobileMenu = document.querySelector(".mobileMenu");
-const buttonClose = document.querySelector(".mobileMenu__nav--menu");
+{
+  const menu = () => {
 
-buttonOpen.addEventListener("click", () => {
-	mobileMenu.classList.add("mobileMenu__open");
-});
+    const buttonOpen = document.querySelector(".header__nav--menu");
+    const mobileMenu = document.querySelector(".mobileMenu");
+    const buttonClose = document.querySelector(".mobileMenu__nav--menu");
 
-buttonClose.addEventListener("click", () => {
-	mobileMenu.classList.remove("mobileMenu__open");
-});
+    buttonOpen.addEventListener("click", () => {
+      mobileMenu.classList.add("mobileMenu__open");
+    });
+
+    buttonClose.addEventListener("click", () => {
+      mobileMenu.classList.remove("mobileMenu__open");
+    });
+  };
+
+
+
+  const bookmarks = () => {
+
+    const simpleButton = document.querySelector(".simpleButton-js");
+    const speedyButton = document.querySelector(".speedyButton-js");
+    const easyButton = document.querySelector(".easyButton-js");
+
+    simpleButton.addEventListener("click", () => {
+      if (!simpleButton.classList.contains("has-underline")) {
+        simpleButton.classList.add("has-underline");
+        speedyButton.classList.remove("has-underline");
+        easyButton.classList.remove("has-underline");
+      };
+    });
+
+
+    speedyButton.addEventListener("click", () => {
+      if (!speedyButton.classList.contains("has-underline")) {
+        simpleButton.classList.remove("has-underline");
+        speedyButton.classList.add("has-underline");
+        easyButton.classList.remove("has-underline");
+      };
+    });
+
+    easyButton.addEventListener("click", () => {
+      if (!easyButton.classList.contains("has-underline")) {
+        easyButton.classList.add("has-underline");
+        speedyButton.classList.remove("has-underline");
+        simpleButton.classList.remove("has-underline");
+      };
+    });
+  }
+
+  const init = () => {
+    menu();
+    bookmarks();
+  }
+
+  init();
+}
